1a1980afbcb425f83c3317972493850e46972818
James Blair <jb8656b@student.american.edu>
2020-04-28 16:26:23 -0400

    The javascript now gets the input and stores it as a variable

--- Homework5_recipe_trivia/recipe-generator/recipe.js
+++ Homework5_recipe_trivia/recipe-generator/recipe.js
@@ -4,5 +4,16 @@
 
   function init(){
     console.log("This is working");
+    let wordButton = document.getElementById("fetch-num");
+    console.log(wordButton);
+    wordButton.addEventListener("click", getWord);
+    getWord();
+  }
+
+  function getWord(){
+    let wordBox = document.getElementById("word-box");
+    let word = wordBox.value;
+    console.log(wordBox);
+    console.log(word);
   }
 })();
